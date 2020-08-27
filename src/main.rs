fdb45c2547721d58c93afabde8a0809623fc86dd
Jabba <jabbathebunny@gmail.com>
2020-08-27 16:10:23 +0100

    initial setup files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+extern crate hyper;
+use hyper::Client;
+
+use structopt::StructOpt;
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// Target url
+    #[structopt(short, long)]
+    url: String,
+
+    /// Set extensions, separated by a comma (E.g. php,html,js)
+    #[structopt(short, long, use_delimiter = true)]
+    extensions: Option<Vec<u16>>,
+
+    /// Select the wordlist you would like to use (Full file path)
+    #[structopt(short, long)]
+    wordlist: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    let url = Opt::from_args();
+    println!("{:?}", url);
+
+    let client = Client::new();
+    let uri = "http://scanme.nmap.org/".parse()?;
+    let resp = client.get(uri).await?;
+    println!("Response: {}", resp.status());
+
+    Ok(())
 }
